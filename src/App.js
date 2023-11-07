0e8a425ac2ecc2bf74de22138e75f1bb678996a7
keyboard-warrior77 <darkmaster4869@gmail.com>
2023-11-07 13:37:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+import uploadIcon from './assets/upload-icon.png';
 
 function App() {
+
+  const [image, setImage] = useState('');
+  const [reUploadButton, setReUploadButton] = useState('button-hidden');
+
+  const inputRef = useRef(null);
+
+  const handleImageChange = () => {
+    inputRef.current.click();
+  }
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+    setReUploadButton('button-visible')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <div className='image-preview' onClick={handleImageChange}>
+          {image ? <img className='after-image-upload' src={URL.createObjectURL(image)} alt='Uploaded' /> :
+            <div className='instructions'>
+              <img src={uploadIcon} alt='Icon' />
+              <h1 style={{ color: "#3498DB" }}>Click here to upload</h1>
+            </div>
+          }
+        </div>
+        <div className='input'>
+          <input type='file' onChange={handleImageUpload} ref={inputRef} accept='image/*' />
+        </div>
+
+        <button className={reUploadButton} onClick={handleImageChange}>Reupload</button>
+      </div>
     </div>
   );
 }
